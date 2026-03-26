5f1d0f23f0748be9aecdcfa17ce6fe41182d1722
Julie T <Jgt58@txstate.edu>
2026-03-26 03:18:30 -0500

    ChatGPT told me i needed to add One more constructor that takes a String when i showed my Position file

--- src/chess/utils/Position.java
+++ src/chess/utils/Position.java
@@ -5,10 +5,19 @@ public class Position{
     private int col;
 
     //note to self: position 8 = row 0, 7 = row 1, etc.
-    
+
     public Position(int row, int col) {
         this.row = row;
         this.col = col;
+    }
+
+    public Position(String pos){
+        char letter = pos.charAt(0);
+        char number = pos.charAt(1);
+
+        this.col = letter - 'A';
+        this.row = 8 - (number - '0'); //because of ASCII
+        letter = Character.toUpperCase(letter);
     }
 
     public int getRow() {
