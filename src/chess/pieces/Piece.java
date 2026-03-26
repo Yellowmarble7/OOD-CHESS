ffd806b71acc718df58a91bbf177f204c82d38d5
Julie T <Jgt58@txstate.edu>
2026-03-26 16:31:21 -0500

    Added the edits to piece.java that chat told me to

--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -5,24 +5,18 @@ import chess.utils.Position;
 
 public abstract class Piece {
     protected String color;
-    protected Position position;
 
-    public Piece(String color, Position position) {
+    public Piece(String color) {
         this.color = color;
-        this.position = position;
     }
 
     public String getColor() {
         return color;
     }
 
-    public Position getPosition() {
-        return position;
-    }
+    public abstract String getSymbol();
 
-    public abstract List<Position> getValidMoves();
-
-    public String getSymbol() {
-        return "";
+    public List<Position> possibleMoves(){
+        return List.of();
     }
 }
