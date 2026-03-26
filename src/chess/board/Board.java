794212f6b81b17a05c4ca989d77110e596b03605
Julie T <Jgt58@txstate.edu>
2026-03-26 15:04:46 -0500

    Added getPiece and movePiece

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -6,3 +6,14 @@ public Board(){
     board = new Piece[8][8];
     setupBoard();
 }
+
+public Piece getPiece(Position pos){
+    return board[pos.getRow()][pos.getCol()];
+}
+
+public Piece movePiece(Position from, Position to){
+    Piece piece = board[from.getRow()][from.getCol()];
+    board[from.getRow()][from.getCol()] = null;
+    board[to.getRow()][to.getCol()] = piece;
+    return piece;
+}
