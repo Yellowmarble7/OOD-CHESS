c3eaf48031b95c99e3264e46d23664a6742ceaad
Julie T <Jgt58@txstate.edu>
2026-03-26 02:46:52 -0500

    added position constructor and getters

--- src/chess/utils/Position.java
+++ src/chess/utils/Position.java
@@ -1,1 +1,21 @@
 package chess.utils;
+
+public class Position{
+    private int row;
+    private int col;
+
+    //note to self: position 8 = row 0, 7 = row 1, etc.
+    
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
