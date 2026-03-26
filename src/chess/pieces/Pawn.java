5fb286134d92837b83ba0c534a58dc211c42fcce
Julie T <Jgt58@txstate.edu>
2026-03-26 16:34:44 -0500

    implemented pawn.java

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,1 +1,12 @@
 package chess.pieces;
+
+public class Pawn extends Piece {
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getSymbol() {
+        return color.equals("white") ? "wp" : "bp";
+    }
+}
