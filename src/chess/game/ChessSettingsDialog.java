399409c1ab5af6cb1bacbfd99615f6e2fadc8251
Julie T <Jgt58@txstate.edu>
2026-04-15 22:18:43 -0500

    p2 added ChessSettingsDialog file

---
+++ src/chess/game/ChessSettingsDialog.java
@@ -0,0 +1,10 @@
+package chess.game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessSettingsDialog extends JDialog {
+
+    private final JComboBox<String> boardThemeBox;
+    private final JComboBox<String> pieceStyleBox;
+    private final JComboBox<String> boardSizeBox;
