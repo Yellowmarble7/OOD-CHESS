7fe5b4458d325f5a71e92c921f1ac58793ac3285
Julie T <Jgt58@txstate.edu>
2026-04-30 01:49:59 -0500

    p3 findKing

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -176,4 +176,19 @@ public boolean isValidMove(Position from, Position to, boolean whiteTurn) {
     return true;
 }
 
+/**
+ * Detects king.
+ */
+public Position findKing(String color) {
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            Piece piece = board[row][col];
+            if (piece instanceof King && piece.getColor().equals(color)) {
+                return new Position(row, col);
+            }
+        }
+    }
+    return null;
+}
+
 }
