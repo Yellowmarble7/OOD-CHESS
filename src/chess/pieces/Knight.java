7182022a3d61fdb449ee4784ef1c81d64073995d
Julie T <Jgt58@txstate.edu>
2026-04-30 01:20:16 -0500

    p3 isValidMove for knight

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,4 +1,8 @@
 package chess.pieces;
+
+import chess.board.Board;
+import chess.utils.Position;
+
 /**
  * Represents a Knight chess piece.
  */
@@ -16,4 +20,15 @@ public class Knight extends Piece {
     public String getSymbol() {
         return color.equals("white") ? "wN" : "bN";
     }
+
+/**
+     * Checks if knight move is valid.
+     */
+    @Override
+public boolean isValidMove(Position from, Position to, Board board) {
+    int rowDiff = Math.abs(from.getRow() - to.getRow());
+    int colDiff = Math.abs(from.getCol() - to.getCol());
+
+    return (rowDiff == 2 && colDiff == 1) || (rowDiff == 1 && colDiff == 2);
+}
 }
