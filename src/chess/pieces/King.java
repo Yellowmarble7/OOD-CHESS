64f14cd2304b3606ff433cc6b728a7a78bbceffc
Julie T <Jgt58@txstate.edu>
2026-04-30 01:25:31 -0500

    p3 isValidMove for King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,4 +1,6 @@
 package chess.pieces;
+import chess.board.Board;
+import chess.utils.Position;
 /**
  * Represents a King chess piece.
  */
@@ -16,4 +18,15 @@ public class King extends Piece {
     public String getSymbol() {
         return color.equals("white") ? "wK" : "bK";
     }
+
+/**
+ * isValidMove for King.
+ */
+    @Override
+public boolean isValidMove(Position from, Position to, Board board) {
+    int rowDiff = Math.abs(from.getRow() - to.getRow());
+    int colDiff = Math.abs(from.getCol() - to.getCol());
+
+    return rowDiff <= 1 && colDiff <= 1 && !(rowDiff == 0 && colDiff == 0);
+}
 }
