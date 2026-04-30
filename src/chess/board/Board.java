66c882eb218d37f21eb35ffea1b030602b39da54
Julie T <Jgt58@txstate.edu>
2026-04-30 02:26:00 -0500

    p3 isInsideBoard

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -268,4 +268,12 @@ public boolean isCheckmate(String color) {
     return true;
 }
 
+/**
+ * Checks if square is valid.
+ */
+public boolean isInsideBoard(Position pos) {
+    return pos.getRow() >= 0 && pos.getRow() < 8 &&
+           pos.getCol() >= 0 && pos.getCol() < 8;
+}
+
 }
